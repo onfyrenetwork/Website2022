b178d331bcc0ad35fe6edb630eb9a23e38c14275
NoahOnFyre <noahonfyre@gmail.com>
2023-05-13 22:31:23 +0200

    Changed UI and design.

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
+const sidebarToggle = document.getElementById("sidebarToggle")
+const sidebar = document.getElementById("sidebar")
+sidebar.style.display = "none"
 
-let gitCommit = document.getElementById("gitCommit")
-
-let aterniumRadio = document.getElementById("aterniumSoftwareRadio")
-let fyutilsRadio = document.getElementById("fyutilsSoftwareRadio")
-let aventurinRadio = document.getElementById("aventurinSoftwareRadio")
-let rhodionitRadio = document.getElementById("rhodionitSoftwareRadio")
-
-let aterniumContainer = document.getElementById("aterniumContainer")
-let fyutilsContainer = document.getElementById("fyutilsContainer")
-let aventurinContainer = document.getElementById("aventurinContainer")
-let rhodionitContainer = document.getElementById("rhodionitContainer")
-
-function sleep(secs) {
-    return new Promise(resolve => setTimeout(resolve, secs*1000));
-}
-
-function getSelectedRadioValueOf(name) {
-    return radioValue = document.querySelector("input[name='" + name + "']:checked").value
-}
-
-function updateSoftwareSection() {
-    if(aterniumRadio.checked) {
-        aterniumContainer.style.display = "flex"
-        fyutilsContainer.style.display = "none"
-        aventurinContainer.style.display = "none"
-        rhodionitContainer.style.display = "none"
-    } else if(fyutilsRadio.checked) {
-        aterniumContainer.style.display = "none"
-        fyutilsContainer.style.display = "flex"
-        aventurinContainer.style.display = "none"
-        rhodionitContainer.style.display = "none"
-    } else if(aventurinRadio.checked) {
-        aterniumContainer.style.display = "none"
-        fyutilsContainer.style.display = "none"
-        aventurinContainer.style.display = "flex"
-        rhodionitContainer.style.display = "none"
-    } else if(rhodionitRadio.checked) {
-        aterniumContainer.style.display = "none"
-        fyutilsContainer.style.display = "none"
-        aventurinContainer.style.display = "none"
-        rhodionitContainer.style.display = "flex"
+function checkSidebar() {
+    if(!sidebarToggle.checked) {
+        sidebar.style.display = "none"
+    } else {
+        sidebar.style.display = "flex"
     }
 }
 
-updateSoftwareSection()
-updateGitFooter()
-
-aterniumRadio.addEventListener("click", updateSoftwareSection)
-fyutilsRadio.addEventListener("click", updateSoftwareSection)
-aventurinRadio.addEventListener("click", updateSoftwareSection)
-rhodionitRadio.addEventListener("click", updateSoftwareSection)
+sidebarToggle.addEventListener("click", checkSidebar)
+checkSidebar()
