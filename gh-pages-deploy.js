2b501036af3ef263bebf40ae50a84b36890f34b2
NoahOnFyre <noahonfyre@gmail.com>
2023-06-01 00:09:40 +0200

    Deploying branch will not be created anymore.

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -3,7 +3,6 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
